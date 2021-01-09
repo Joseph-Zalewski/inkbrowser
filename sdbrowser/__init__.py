92de8f546df3bc7da79b4c117ddea51e97524e28
Joseph Zalewski <jzalewski@ksu.edu>
2021-01-09 15:06:38 -0600

    Initial commit of Stage 1. Only has __init__.py with a Hello World test.

---
+++ sdbrowser/__init__.py
@@ -0,0 +1,21 @@
+import os
+from flask import Flask
+
+#__init__ file for Stage 1, which is only intended to be able to
+#use SPARQLWrapper successfully in some way.
+
+#This is Flask app factory boilerplate. No value is configured to
+#DATABASE (a property of <<<app>>>), because this simple application doesn't
+#need to store data on the server. We don't really need the instance folder
+#at all.
+def create_app():
+    app = Flask(__name__,instance_relative_config = True)
+    app.config.from_mapping(
+        SECRET_KEY = 'dev'
+    )
+        
+    @app.route('/test')
+    def hello():
+        return 'Hello, World!'
+            
+    return app
